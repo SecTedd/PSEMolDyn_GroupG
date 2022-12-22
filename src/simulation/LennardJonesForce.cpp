83f39ca1cdcd0b0bc04021679b3d6ea2a64a56e1
Michael Borisov <ge96jab@mytum.de>
2022-12-22 18:47:01 +0100

    code relatet improvements

--- src/simulation/LennardJonesForce.cpp
+++ src/simulation/LennardJonesForce.cpp
@@ -34,7 +34,11 @@ void LennardJonesForce::calculateForce(ParticleContainer &particleContainer)
     {
         double distance = ArrayUtils::L2Norm(p1.getX() - p2.getX());
         double sigma = (p1.getSigma() + p2.getSigma()) / 2;
-        double epsilon = sqrt(p1.getEpsilon() * p2.getEpsilon());       
+        double epsilon;  
+        if(p1.getEpsilon() == p2.getEpsilon())
+            epsilon = p1.getEpsilon(); 
+        else   
+            epsilon = sqrt(p1.getEpsilon() * p2.getEpsilon()); 
 
         // Reduce number of operation by reusing previous results
         double pow1 = sigma / distance;
